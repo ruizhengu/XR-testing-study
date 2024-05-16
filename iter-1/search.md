9e63fedd5943ee7deb467d3ea1b539b30ac0c22d
Ruizhen Gu <ravenna1031@gmail.com>
2024-05-16 15:45:09 +0100

    add keyword verification

--- iter-1/search.md
+++ iter-1/search.md
@@ -32,9 +32,12 @@ Search String = $XR **AND** ($T **OR** $B) **AND** $S **NOT** $UX
 
 ```
 # title
-("virtual reality" OR "augmented reality" OR "mixed reality" OR "extended reality" OR VR OR AR OR XR OR MR) AND (test OR validation OR verification OR bug OR defect OR fault OR error) NOT usability
+("virtual reality" OR "augmented reality" OR "mixed reality" OR "extended reality" OR “VR” OR “AR” OR “XR” OR “MR”) AND (test OR validation OR verification OR bug OR defect OR fault OR error) NOT usability
 
 # full text
 software OR application
+
+#IEEE
+https://ieeexplore.ieee.org/search/searchresult.jsp?action=search&newsearch=true&matchBoolean=true&queryText=(%22Document%20Title%22:%22virtual%20reality%22%20OR%20%22Document%20Title%22:%22augmented%20reality%22%20OR%20%22Document%20Title%22:%22mixed%20reality%22%20OR%20%22Document%20Title%22:%22extended%20reality%22%20OR%20%22Document%20Title%22:VR%20OR%20%22Document%20Title%22:AR%20OR%20%22Document%20Title%22:XR%20OR%20%22Document%20Title%22:MR)%20AND%20(%22Document%20Title%22:test%20OR%20%22Document%20Title%22:validation%20OR%20%22Document%20Title%22:verification%20OR%20%22Document%20Title%22:bug%20OR%20%22Document%20Title%22:defect%20OR%20%22Document%20Title%22:fault%20OR%20%22Document%20Title%22:error)%20NOT%20(%22Document%20Title%22:usability)%20AND%20(%22Full%20Text%20Only%22:software%20OR%20%22Full%20Text%20Only%22:application)
 ```
 
--- metadata.py
+++ metadata.py
@@ -7,7 +7,8 @@ import glob
 class Metadata:
     def __init__(self):
         # self.bib_files = glob.glob(os.path.join("iter-1/bib", '*.bib'))
-        self.bib_files = glob.glob(os.path.join("iter-1/bib", 'IEEE*.bib'))
+        # self.bib_files = glob.glob(os.path.join("iter-1/bib", 'IEEE*.bib'))
+        self.bib_files = glob.glob(os.path.join("iter-1/bib", 'scopus*.bib'))
         self.excel_file = "iter-1/XR Testing Literature.xlsx"
         self.headers = [
             "Title",
@@ -18,13 +19,26 @@ class Metadata:
             "Booktitle",
             "Keywords"
         ]
-        if os.path.exists(self.excel_file):
-            self.workbook = load_workbook(self.excel_file)
-            self.sheet = self.workbook["IEEE"]
-        else:
-            self.workbook = Workbook()
-            self.sheet = self.workbook.active
-        self.sheet.append(self.headers)
+        # if os.path.exists(self.excel_file):
+        #     self.workbook = load_workbook(self.excel_file)
+        #     self.sheet = self.workbook["IEEE"]
+        # else:
+        #     self.workbook = Workbook()
+        #     self.sheet = self.workbook.active
+        # self.sheet.append(self.headers)
+
+    def keyword_verification(self):
+        count = 0
+        for bib_file in self.bib_files:
+            with open(bib_file) as file:
+                bib_data = bibtexparser.load(file)
+            for entry in bib_data.entries:
+                title = entry.get("title", "N/A")
+                if (
+                        "virtual reality" in title or "augmented reality" in title or "mixed reality" or "extended reality" in title or "VR" in title or "AR" in title or "XR" in title or "MR" in title) and (
+                        "test" in title or "validation" in title or "verification" in title or "bug" in title or "defect" in title or "fault" in title or "error" in title) and "usability" not in title:
+                    count += 1
+        print(count)
 
     def save_metadata(self):
         for bib_file in self.bib_files:
@@ -54,4 +68,5 @@ class Metadata:
 
 if __name__ == '__main__':
     m = Metadata()
-    m.save_metadata()
+    # m.save_metadata()
+    m.keyword_verification()
